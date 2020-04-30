3ac7205f1b2f48ea9e521239b84514b0b40cfe57
Brandy Mello <brandyamello@gmail.com>
2020-04-30 14:34:57 -0600

    Create a react app and add sass

---
+++ farmers-market-shop/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>Farmers' Market Shop</h1>
+    )
+  }
+}
+
+export default App;
