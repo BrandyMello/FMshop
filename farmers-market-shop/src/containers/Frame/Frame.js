e002888d7af9728a5c1b00129241f6f85af6c151
Brandy Mello <brandyamello@gmail.com>
2020-05-01 12:27:34 -0600

    Add fonts nova square for heading and open sans for content and import logo which needs work

--- farmers-market-shop/src/containers/Frame/Frame.js
+++ farmers-market-shop/src/containers/Frame/Frame.js
@@ -1,11 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import Logo from "../../assets/FarmersMarketShopLogo-Beet1.jpeg";
 import "./Frame.scss";
 import '../App/App.scss';
 
 
+
 const Frame = () => {
   
-    return <h1>Farmers' Market Shop</h1>;
+    return (
+      <header>
+        <img src={ Logo } alt=''/>
+        <h1>Farmers' Market Shop</h1>
+        <p>Apparently we had reached a great h</p>
+      </header>
+    );
 }
 
 export default Frame;
