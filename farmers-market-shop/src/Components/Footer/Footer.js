35dec20511a7caaad68b80c815c86c824597ab73
Brandy Mello <brandyamello@gmail.com>
2020-06-04 14:01:28 -0600

    Modularize Frame by breaking out the header with the nav and footer still wondering wether to break out the nav as well

---
+++ farmers-market-shop/src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { MdCopyright } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>
+          <MdCopyright />
+          VelvetPlaidProductions
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
